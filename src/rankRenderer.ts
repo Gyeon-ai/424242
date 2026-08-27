48f384ce8dac44c82984a9eb1eba8b0341bde413
Gyeon-ai <Gyeon-ai@users.noreply.github.com>
2026-08-27 17:19:29 +0900

    Use crown icon for winner

--- src/rankRenderer.ts
+++ src/rankRenderer.ts
@@ -89,8 +89,8 @@ export class RankRenderer implements UIObject {
       const y = rank * this.fontHeight;
       if (y >= startY && y <= startY + ctx.canvas.height) {
         ctx.fillStyle = `hsl(${marble.hue} 100% ${theme.marbleLightness}`;
-        ctx.strokeText(`${rank === winnerRank ? '☆' : '\u2714'} ${marble.name} #${rank + 1}`, startX, 20 + y);
-        ctx.fillText(`${rank === winnerRank ? '☆' : '\u2714'} ${marble.name} #${rank + 1}`, startX, 20 + y);
+        ctx.strokeText(`${rank === winnerRank ? '👑' : '\u2714'} ${marble.name} #${rank + 1}`, startX, 20 + y);
+        ctx.fillText(`${rank === winnerRank ? '👑' : '\u2714'} ${marble.name} #${rank + 1}`, startX, 20 + y);
       }
     });
     ctx.font = `500 10pt ${UI_FONT_FAMILY}`;
